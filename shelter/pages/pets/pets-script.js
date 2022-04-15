76bc9030ed3b1a0402c04427ed7d4597bc17ca0d
Safonova Alexandra <alexa-09@bk.ru>
2022-04-15 23:39:33 +0300

    fix burger menu

--- shelter/pages/pets/pets-script.js
+++ shelter/pages/pets/pets-script.js
@@ -2,18 +2,18 @@ const petBurger = document.querySelector('.pets-burger');
 const petNavigation = document.querySelector('.pets-header__navigation');
 const shadow = document.querySelector('.shadow');
 const body = document.querySelector('body');
-//const logo = document.querySelector('.logo__wrapper');
+const logo = document.querySelector('.logo__wrapper');
 
 function toggleMenu() {
     petBurger.classList.toggle('open');
     petNavigation.classList.toggle('open');
     shadow.classList.toggle('open');
-    //logo.classList.toggle('open');
+    logo.classList.toggle('open');
     body.classList.toggle('lock');
     }
 
 petBurger.addEventListener('click', toggleMenu);
 petNavigation.addEventListener('click', toggleMenu);
 shadow.addEventListener('click', toggleMenu);
-//logo.addEventListener('click', toggleMenu);
+logo.addEventListener('click', toggleMenu);
 body.addEventListener(toggleMenu);
